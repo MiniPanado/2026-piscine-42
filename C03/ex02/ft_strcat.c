baaa65e3a654892b9df7ebbefea87c56d2fb46d1
MiniPanado <lucasdarochacerqueira18@gmail.com>
2026-07-27 09:58:48 +0100

    Until C03 ex02 is done

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,16 @@
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (dest[len] != '\0')
+		len++;
+	while (src[i] != '\0')
+	{
+		dest[len++] = src[i++];
+	}
+	dest[len] = '\0';
+	return (dest);
+}
