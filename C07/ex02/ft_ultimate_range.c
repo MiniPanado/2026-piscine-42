9bb0edb0be10ab40aff792ab792df5150bf59a55
MiniPanado <lucasdarochacerqueira18@gmail.com>
2026-08-05 02:31:31 +0100

    Ex02 corrected

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	const size_t	size = max - min;
+	size_t			i;
+
+	if (range == NULL)
+	{
+		return (-1);
+	}
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = (int *)malloc(size * sizeof(int));
+	if (*range == NULL)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (i < size)
+	{
+		*(*range + i) = (min + i);
+		i++;
+	}
+	return (size);
+}
+
+/*#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	int	*range;
+	if (argc != 3)
+	{
+		printf("You need to pass 2 arguments (Min and Max)!\n");
+		return (1);
+	}
+	int min = atoi(argv[1]);
+	int max = atoi(argv[2]);
+	int ret = ft_ultimate_range(&range, min, max);
+	if (!range)
+	{
+		printf("Min can't be greater than max\n");
+		return (1);
+	}
+	int size = abs(max - min);
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", range[i]);
+	}
+	printf("\nTotal of Numbers: %d\n", ret);
+	free(range);
+}*/
